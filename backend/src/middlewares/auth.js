6f12e5027b044b05f5ffcb93371f2b1d8916bfe2
Cauã Henrique <cauah6002@gmail.com>
2020-11-28 15:23:16 -0300

    Adicionando middleware que verifica se o usuário está logado

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+require("dotenv-safe").config();
+
+module.exports = middleware => {
+    return (req, res, next) => {
+        if(req.headers.token){
+            jwt.verify(req.headers.token, process.env.SECRET, (error, encoded) => {
+                if(error){
+                    return res.json({ error: true, message: 'Token Inválido!' })
+                }
+                else{
+                    middleware(req, res, next)
+                }
+            })
+        }
+        else{
+            res.json({ error: true, message: 'Usuário não está autenticado' })
+        }
+    }
+}
